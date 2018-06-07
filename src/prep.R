f2c971a40fdbe841b47bab23ba5182f4c281ab25
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-07 16:38:08 -0600

    Update indentation in prep.annotation.pkg.name

--- src/prep.R
+++ src/prep.R
@@ -1,13 +1,13 @@
 # This module contains utilities required for data preparation.
 prep.annotation.pkg.name <- function(gene.annotation){
-    # Append gene ".db" to the annotation package name, if required.
-    #
-    # Args:
-    # gene.annotation: annotation package name with or without .db extenstion.
-    #
-    # Returns:
-    # An string representing annotation package name with .db extenstion
-    
+  # Append gene ".db" to the annotation package name, if required.
+  #
+  # Args:
+  #   gene.annotation: annotation package name with or without .db extenstion.
+  #
+  # Returns:
+  #   An string representing annotation package name with .db extenstion
+  
   if(gene.annotation %in% c(NULL, NA, ""))
      stop("Annotation name is absent!")
   if(!endsWith(gene.annotation, ".db"))

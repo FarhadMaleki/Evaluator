02c8b6a2e05587074a5f3862f383fd0964f6d176
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-15 01:14:06 -0600

    Implement FRYWrapper and test it

--- src/wrappers.R
+++ src/wrappers.R
@@ -467,3 +467,52 @@ run.ROASTWrapper <- function(obj, multitest.adjustment="BH", sort.result=TRUE,
   return(roast.results)
 }
 ###############################################################################
+##                              FRYWrapper                                ##
+###############################################################################
+FRYWrapper <- function(expression.set, genesets, contrast){
+  # Constructor for FRYWrapper
+  #
+  # Args:
+  #   expression.set: An ExpressionSet object (see GSEABase package).
+  #   genesets: A list of gene sets.
+  #   contrast: A vector like representing case and control samples. Control 
+  #     samples should be represented by "c" and case sample should be
+  #     represented by "d".
+  # Return:
+  #   A FRYWrapper object that is a list of expression.set, genesets, and
+  #     contrast.
+  obj <- assemble.obj(expression.set, genesets, contrast)
+  class(obj) <- "FRYWrapper"
+  return(obj)
+}
+###############################################################################
+# define run method for FRYWrapper
+run.FRYWrapper <- function(obj, multitest.adjustment="BH", sort.result=TRUE,
+                           ...){
+  # Run method for FRYWrapper objects
+  # 
+  # Args:
+  #   obj: A FRYWrapper object created by FRYWrapper.
+  #   multitest.adjustment: Adjustment for multiple comparisons (see p.adjust).
+  #   sort.result: Logical, True to sort the result based on adjusted p-values.
+  #   ...: see the documentation for roast method from limma package.
+  # Returns:
+  #   A data.frame representing the result of gene set analysis.
+  require("limma") || stop("Package limma is not available!")
+  # Run roast
+  group <- factor(obj$contrast)
+  design.matrix <- model.matrix(~ group)
+  design.matrix.col <- 2
+  fry.results <- fry(y=exprs(obj$expression.set),
+                     index = obj$genesets,
+                     design = design.matrix,
+                     contrast = design.matrix.col)
+  colnames(fry.results)[which(colnames(fry.results) == "PValue")] = "p.value"
+  fry.results$FDR <- NULL
+  fry.results$p.adj <- p.adjust(fry.results$p.value,
+                                  method = multitest.adjustment)
+  if(sort.result)
+    fry.results <- fry.results[order(fry.results[,"p.adj"]),]
+  return(fry.results)
+}
+###############################################################################
--- test/testWrappers.R
+++ test/testWrappers.R
@@ -153,3 +153,23 @@ test_that("ROASTWrapper works as expected", {
   expect_true(results["Geneset4", "p.adj"] < 0.05)
 }
 )
+
+test_that("FRYWrapper works as expected", {
+  data <- load.dummy.dataset()
+  contrast <- data$contrast
+  expression.set <- data$expression.set
+  genesets <- data$genesets
+  fry.wrapper <- FRYWrapper(expression.set, genesets, contrast)
+  results <- run(fry.wrapper, multitest.adjustment="BH",
+                 sort.result=TRUE)
+  print(results)
+  expect_equal(dim(results), c(5, 6))
+  expect_true("p.value" %in% colnames(results))
+  expect_true("p.adj" %in% colnames(results))
+  expect_true(results["Geneset1", "p.adj"] < 0.05)
+  expect_true(results["Geneset4", "p.adj"] < 0.05)
+  expect_true(results["Geneset2", "p.adj"] > 0.05)
+  expect_true(results["Geneset3", "p.adj"] > 0.05)
+  expect_true(results["Geneset5", "p.adj"] > 0.05)
+}
+)

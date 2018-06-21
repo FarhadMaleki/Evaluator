2c0651aa22ca90d9233af649983242478c7fc641
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-21 00:36:17 -0600

    Add more tests for testing SSGSEAWrapper

--- test/testSSGSEAWrapper.R
+++ test/testSSGSEAWrapper.R
@@ -5,10 +5,8 @@ source("loadDummyDatasets.R")
 
 test_that("SSGSEAWrapper works as expected", {
   data <- load.dummy.dataset()
-  annotation <- data$annotation
   contrast <- data$contrast
   expression.set <- data$expression.set
-  background <- data$background
   genesets <- data$genesets
   ssgsea.wrapper <- SSGSEAWrapper(expression.set, genesets, contrast)
   results <- run(ssgsea.wrapper, multitest.adjustment="BH", sort.result=TRUE)
@@ -17,6 +15,16 @@ test_that("SSGSEAWrapper works as expected", {
   expect_true("p.adj" %in% colnames(results))
   expect_false("P.Value" %in% colnames(results))
   expect_false("adj.P.Val" %in% colnames(results))
-
+  data <- load.dummy.dataset(idx=1)
+  contrast <- data$contrast
+  expression.set <- data$expression.set
+  genesets <- data$genesets
+  ssgsea.wrapper <- SSGSEAWrapper(expression.set, genesets, contrast)
+  results <- run(ssgsea.wrapper, multitest.adjustment="BH", sort.result=TRUE)
+  expect_equal(dim(results), c(5,5))
+  expect_true("p.value" %in% colnames(results))
+  expect_true("p.adj" %in% colnames(results))
+  expect_false("P.Value" %in% colnames(results))
+  expect_false("adj.P.Val" %in% colnames(results))
 }
 )

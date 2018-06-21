8901aa00a3766b50f194d84b298ffeea5fe470b4
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-20 21:48:02 -0600

    Update load.dummy.dataset to load another dataset

--- test/testWrappers.R
+++ test/testWrappers.R
@@ -2,11 +2,27 @@ context("Test prep")
 source("../src/wrappers.R")
 source("../src/prep.R")
 source("../src/GSEA.1.0.R")
-load.dummy.dataset <-function(){
+load.dummy.dataset <-function(idx=0){
   # Load data set dummy dataset
+  #
+  # Args:
+  #   idx: 0 or 1, representing the version of expression dataset
+  # Returns:
+  #   A list consist of an artificial dataset containing
+  #     - annotation
+  #     - contrast
+  #     - expression.set
+  #     - background
+  #     - genesets
+
+
   data <- list()
   geneset.collection.address <- "../data/GenesetCollectionGS1-5/OriginalGS1-GS5.gmt"
-  profile.address <- "../data/ArtificialExpressionData/ArtificialExpressionProfile.csv"
+  if(idx == 0){
+    profile.address <- "../data/ArtificialExpressionData/ArtificialExpressionProfile.csv"
+    }else if(idx == 1){
+    profile.address <- "../data/ArtificialExpressionData/ArtificialExpressionProfile1.txt"
+  }
   geneset.collection <- prep.loadGMT(geneset.collection.address)
   data$annotation <- "hu6800"
   data$contrast <- c("c", "c", "c", "c", "c", "d", "d", "d", "d", "d")

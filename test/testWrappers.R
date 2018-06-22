133f5458713c470d7a5a4a71d2221bacf26066d0
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-22 10:03:50 -0600

    Refactor tests for GlobalTestWrapper
    Tests for this function is moved to testGlobalTestWrapper.R file.

--- test/testWrappers.R
+++ test/testWrappers.R
@@ -5,25 +5,6 @@ source("../src/GSEA.1.0.R")
 source("loadDummyDatasets.R")
 
 
-test_that("GlobalTestWrapper works as expected", {
-  data <- load.dummy.dataset()
-  contrast <- data$contrast
-  expression.set <- data$expression.set
-  genesets <- data$genesets
-  gt.wrapper <- GlobalTestWrapper(expression.set, genesets, contrast)
-  results <- run(gt.wrapper, multitest.adjustment="BH",
-                 num.permutation=1000, sort.result=TRUE)
-  expect_equal(dim(results), c(5, 6))
-  expect_true("p.value" %in% colnames(results))
-  expect_true("p.adj" %in% colnames(results))
-  expect_true(results["Geneset1", "p.adj"] < 0.05)
-  expect_true(results["Geneset4", "p.adj"] < 0.05)
-  expect_true(results["Geneset2", "p.adj"] > 0.05)
-  expect_true(results["Geneset3", "p.adj"] > 0.05)
-  expect_true(results["Geneset5", "p.adj"] > 0.05)
-}
-)
-
 test_that("GAGEWrapper works as expected", {
   data <- load.dummy.dataset()
   contrast <- data$contrast

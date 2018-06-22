a4657b3243f4a09f103e261542785c6414e46145
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-22 11:33:02 -0600

    Refactor tests for GAGEWrapper
    Tests for this function is moved to testGAGEWrapper.R file.

--- test/testWrappers.R
+++ test/testWrappers.R
@@ -5,26 +5,6 @@ source("../src/GSEA.1.0.R")
 source("loadDummyDatasets.R")
 
 
-test_that("GAGEWrapper works as expected", {
-  data <- load.dummy.dataset()
-  contrast <- data$contrast
-  expression.set <- data$expression.set
-  genesets <- data$genesets
-  gage.wrapper <- GAGEWrapper(expression.set, genesets, contrast)
-  results <- run(gage.wrapper, multitest.adjustment="BH",
-                 num.permutation=1000, sort.result=TRUE,
-                 same.dir=FALSE, compare="unpaired")
-  expect_equal(dim(results), c(5, 5))
-  expect_true("p.value" %in% colnames(results))
-  expect_true("p.adj" %in% colnames(results))
-  expect_true(results["Geneset1", "p.adj"] < 0.05)
-  expect_true(results["Geneset4", "p.adj"] < 0.05)
-  expect_true(results["Geneset2", "p.adj"] > 0.05)
-  expect_true(results["Geneset3", "p.adj"] > 0.05)
-  expect_true(results["Geneset5", "p.adj"] > 0.05)
-}
-)
-
 test_that("GSAWrapper works as expected", {
   data <- load.dummy.dataset()
   contrast <- data$contrast

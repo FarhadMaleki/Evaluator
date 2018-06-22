a33e3717a6db0611e8d3ff001f315a08a8a462a5
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-22 10:08:17 -0600

    Add more tests for testing GlobalTestWrapper

--- test/testGlobalTestWrapper.R
+++ test/testGlobalTestWrapper.R
@@ -20,5 +20,21 @@ test_that("GlobalTestWrapper works as expected", {
   expect_true(results["Geneset2", "p.adj"] > 0.05)
   expect_true(results["Geneset3", "p.adj"] > 0.05)
   expect_true(results["Geneset5", "p.adj"] > 0.05)
+  # Testing GlobalTestWrapper with another expression profile
+  data <- load.dummy.dataset(idx=1)
+  contrast <- data$contrast
+  expression.set <- data$expression.set
+  genesets <- data$genesets
+  gt.wrapper <- GlobalTestWrapper(expression.set, genesets, contrast)
+  results <- run(gt.wrapper, multitest.adjustment="BH",
+                 num.permutation=1000, sort.result=TRUE)
+  expect_equal(dim(results), c(5, 6))
+  expect_true("p.value" %in% colnames(results))
+  expect_true("p.adj" %in% colnames(results))
+  expect_true(results["Geneset1", "p.adj"] < 0.05)
+  expect_true(results["Geneset4", "p.adj"] > 0.05)
+  expect_true(results["Geneset2", "p.adj"] > 0.05)
+  expect_true(results["Geneset3", "p.adj"] > 0.05)
+  expect_true(results["Geneset5", "p.adj"] > 0.05)
 }
 )

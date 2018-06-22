522a1e896ac09647e38e63234508dc781328f615
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-22 12:22:14 -0600

    Add more tests for PADOGWrapper

--- test/testPADOGWrapper.R
+++ test/testPADOGWrapper.R
@@ -31,5 +31,31 @@ test_that("PADOGWrapper works as expected", {
   expect_true(results["Geneset4", "p.adj"] < results["Geneset2", "p.adj"])
   expect_true(results["Geneset4", "p.adj"] < results["Geneset3", "p.adj"])
   expect_true(results["Geneset4", "p.adj"] < results["Geneset5", "p.adj"])
+  # Testing with another expression profile
+  data <- load.dummy.dataset(idx=1)
+  contrast <- data$contrast
+  expression.set <- data$expression.set
+  genesets <- data$genesets
+  padog.wrapper <- PADOGWrapper(expression.set, genesets, contrast)
+  results <- run(padog.wrapper,
+                 multitest.adjustment="BH",
+                 sort.result=TRUE,
+                 paired = FALSE,
+                 block = NULL,
+                 targetgs = NULL,
+                 gslist = genesets,
+                 Nmin = 3,
+                 NI = 1000,
+                 plots = FALSE,
+                 dseed = 123456,
+                 verbose = FALSE)
+  expect_equal(dim(results), c(5, 8))
+  expect_true("p.value" %in% colnames(results))
+  expect_true("p.adj" %in% colnames(results))
+  expect_true(results["Geneset1", "p.adj"] < 0.05)
+  expect_true(results["Geneset1", "p.adj"] < results["Geneset2", "p.adj"])
+  expect_true(results["Geneset1", "p.adj"] < results["Geneset3", "p.adj"])
+  expect_true(results["Geneset1", "p.adj"] < results["Geneset4", "p.adj"])
+  expect_true(results["Geneset1", "p.adj"] < results["Geneset5", "p.adj"])
 }
 )

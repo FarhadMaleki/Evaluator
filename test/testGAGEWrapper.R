7f1adc93d2a9243fa7417e8def0c7b463d3b32a3
Farhad Maleki <farhad.maleki@usask.ca>
2018-06-22 11:40:17 -0600

    Add more tests for GAGEWrapper

--- test/testGAGEWrapper.R
+++ test/testGAGEWrapper.R
@@ -21,5 +21,22 @@ test_that("GAGEWrapper works as expected", {
   expect_true(results["Geneset2", "p.adj"] > 0.05)
   expect_true(results["Geneset3", "p.adj"] > 0.05)
   expect_true(results["Geneset5", "p.adj"] > 0.05)
+  # Testing with another expression profile
+  data <- load.dummy.dataset(idx=1)
+  contrast <- data$contrast
+  expression.set <- data$expression.set
+  genesets <- data$genesets
+  gage.wrapper <- GAGEWrapper(expression.set, genesets, contrast)
+  results <- run(gage.wrapper, multitest.adjustment="BH",
+                 num.permutation=1000, sort.result=TRUE,
+                 same.dir=FALSE, compare="unpaired")
+  expect_equal(dim(results), c(5, 5))
+  expect_true("p.value" %in% colnames(results))
+  expect_true("p.adj" %in% colnames(results))
+  expect_true(results["Geneset1", "p.adj"] < 0.05)
+  expect_true(results["Geneset4", "p.adj"] > 0.05)
+  expect_true(results["Geneset2", "p.adj"] > 0.05)
+  expect_true(results["Geneset3", "p.adj"] > 0.05)
+  expect_true(results["Geneset5", "p.adj"] > 0.05)
 }
 )
